b6f99050085422abbdd8f339ce6de142bdb4817b
Fulop Zoltan <fulopzoltan20@gmail.com>
2021-10-31 22:41:37 +0200

    Finish

--- app/src/main/java/com/example/android_lab3quiz/QuestionFragment.kt
+++ app/src/main/java/com/example/android_lab3quiz/QuestionFragment.kt
@@ -1,11 +1,17 @@
 package com.example.android_lab3quiz
 
+import android.content.Context
+import android.content.DialogInterface
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import com.example.android_lab3quiz.databinding.QuestionFragmentBinding
 import quiz.QuizViewModel
 
@@ -17,28 +23,73 @@ class QuestionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(QuizViewModel::class.java)
+        viewModel = ViewModelProvider(requireActivity()).get(QuizViewModel::class.java)
         binding = QuestionFragmentBinding.inflate(inflater, container, false)
+        viewModel.currentQuestion.observe(requireActivity()) { (question, isLast) ->
+            binding.textViewQuestion.text = question?.text
+            binding.radioButtonA.text = question?.answers?.get(0) ?: "A"
+            binding.radioButtonB.text = question?.answers?.get(1) ?: "B"
+            binding.radioButtonC.text = question?.answers?.get(2) ?: "C"
+            binding.radioButtonD.text = question?.answers?.get(3) ?: "D"
+            if (isLast) {
+                binding.buttonNext.text = "SUBMIT"
+                binding.buttonNext.setOnClickListener {
+                    val fragmentManager = parentFragmentManager.beginTransaction()
+                    fragmentManager.replace(R.id.fragment_container_view, QuizEndFragment())
+                    fragmentManager.commit()
+                }
+            }
+        }
         return binding.root;
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        onNextClick()
-        binding.buttonNext.setOnClickListener { onNextClick() }
+        binding.buttonNext.text = "NEXT"
+        binding.buttonNext.setOnClickListener {
+            val checkedId = binding.radio.checkedRadioButtonId;
+            if (checkedId == -1) {
+                return@setOnClickListener
+            }
+            if (requireView().findViewById<RadioButton>(checkedId).text == viewModel.currentQuestion.value?.first?.correctAnswer) {
+                viewModel.numCorrect++
+
+            }
+            viewModel.getNextQuestion()
+            binding.radio.clearCheck()
+            Log.d("Question", viewModel.numCorrect.toString())
+        }
     }
 
-    private fun onNextClick() {
-        if (viewModel.currentQuestion.hasNext()) {
-            val question = viewModel.currentQuestion.next();
-            binding.textViewQuestion.text = question.text;
-            if (!viewModel.currentQuestion.hasNext()) binding.buttonNext.text = "Finish"
-        } else {
-            val fragmentTransaction = parentFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.fragment_container_view, QuizStartFragment());
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit()
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(
+            true
+        ) {
+            override fun handleOnBackPressed() {
+                alertUser()
+            }
         }
+        requireActivity().onBackPressedDispatcher.addCallback(
+            requireActivity(),
+            callback
+        )
+    }
+
+    fun alertUser() {
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setTitle("WARNING!")
+        builder.setMessage("Are you sure you want to finish the quiz? Your progress will be lost!")
+        builder.setPositiveButton("Yes") { dialog, which ->
+            viewModel.resetQuiz()
+            val fragmentManager = parentFragmentManager.beginTransaction()
+            fragmentManager.replace(R.id.fragment_container_view, QuizStartFragment())
+            fragmentManager.commit()
+        }
+        builder.setNegativeButton("No, I want to continue") { dialog, which ->
+            dialog.cancel()
+        }
+        builder.show()
     }
 }
 
--- app/src/main/java/quiz/QuizViewModel.kt
+++ app/src/main/java/quiz/QuizViewModel.kt
@@ -1,12 +1,23 @@
 package quiz
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
 
 
 class QuizViewModel : ViewModel() {
     val questions: MutableList<Question> = quiz.questions;
-    val correctAnswers: Int = 0
-    var currentQuestion: MutableIterator<Question> = doQuiz(5).iterator()
+    var numCorrect: Int = 0
+    private val numberOfQuestions: Int = 5
+    var itQuestion: MutableIterator<Question> = doQuiz(numberOfQuestions).iterator()
+    var currentQuestion: MutableLiveData<Pair<Question?, Boolean>> =
+        MutableLiveData<Pair<Question?, Boolean>>()
+
+    init {
+        getNextQuestion()
+    }
+
     private fun randomizeQuestions() {
         questions.forEach { q -> q.answers.shuffle() }
         questions.shuffle()
@@ -15,10 +26,20 @@ class QuizViewModel : ViewModel() {
     fun doQuiz(numOfQuestions: Int): MutableList<Question> {
         this.randomizeQuestions()
         val questionsToBeShown = questions.subList(0, numOfQuestions);
-        return questionsToBeShown;
+        return questionsToBeShown
+    }
+
+    fun getNextQuestion() {
+        if (itQuestion.hasNext()) {
+            currentQuestion.value = itQuestion.next() to !itQuestion.hasNext()
+        }
+    }
+
+    fun resetQuiz() {
+        numCorrect = 0
+        itQuestion = doQuiz(5).iterator()
     }
 }
-
 
 
 
